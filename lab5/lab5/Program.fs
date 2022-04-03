48141bf0e9ce0d54f219371c0350a6526e06630d
Дмитрий Александрович Худокормов <s0163526@edu.kubsu.ru>
2022-04-03 11:21:23 +0300

    task 14

--- lab5/lab5/Program.fs
+++ lab5/lab5/Program.fs
@@ -38,46 +38,70 @@ let main argv =
     0
     *)
 
-    (* 13.
-    let rec proizved n = 
-        if n=0 then 1
-        else n%10*proizved(n/10)
-    
-    let max num = 
-        let rec max1 num maxCipher =
-            if num = 0 then maxCipher
+(* 13.
+let rec proizved n = 
+    if n=0 then 1
+    else n%10*proizved(n/10)
+
+let max num = 
+    let rec max1 num maxCipher =
+        if num = 0 then maxCipher
+        else
+            let nM = (num%10)
+            let nDel = (num/10)
+            if(nM>maxCipher) then max1 nDel nM
             else
-                let nM = (num%10)
-                let nDel = (num/10)
-                if(nM>maxCipher) then max1 nDel nM
-                else
-                    max1 nDel maxCipher
-        max1 num 0
+                max1 nDel maxCipher
+    max1 num 0
+    
+let min num = 
+    let rec min1 num minCipher =
+        if num = 0 then minCipher
+        else
+            let nM = (num%10)
+            let nDel = (num/10)
+            if(nM<minCipher) then min1 nDel nM
+            else
+                min1 nDel minCipher
+    min1 num 10
+
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите число: ")
+    let n = Convert.ToInt32(Console.ReadLine())
+    let maxOut = max>>printfn "%i"
+    let proizvedOut = proizved>>printfn "%i"
+    let minOut = min>>printfn "%i"
+    Console.WriteLine("Max ciph: ")
+    maxOut n
+    Console.WriteLine("Proizved ciph: ")
+    proizvedOut n
+    Console.WriteLine("Min ciph: ")
+    minOut n
+    0
+    *)
+
+(* 14.
+
+let obhodDelit num f defaultZnach = 
+    let rec obhodDelitPodKapotom basicNum f defZn currentNum =
+        let resNum = currentNum-1
+        if currentNum = 0 then defZn
+        elif((basicNum%currentN) = 0) 
+        then
+             let defZn2 = (f defZn currentNum) 
+             obhodDelitPodKapotom basicNum f defZn2 resNum
+        else obhodDelitPodKapotom basicNum f defZn resNum
+    obhodDelitPodKapotom num f defaultZnach num
         
-    let min num = 
-        let rec min1 num minCipher =
-            if num = 0 then minCipher
-            else
-                let nM = (num%10)
-                let nDel = (num/10)
-                if(nM<minCipher) then min1 nDel nM
-                else
-                    min1 nDel minCipher
-        min1 num 10
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите число")
+    let num = Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Сумма делителей числа равна: ")
+    printfn "%i" (obhodDelit num (fun x y -> x+y)  0)
+    0 
+    *)
     
-    
-    [<EntryPoint>]
-    let main argv =
-        Console.WriteLine("Введите число: ")
-        let n = Convert.ToInt32(Console.ReadLine())
-        let maxOut = max>>printfn "%i"
-        let proizvedOut = proizved>>printfn "%i"
-        let minOut = min>>printfn "%i"
-        Console.WriteLine("Max ciph: ")
-        maxOut n
-        Console.WriteLine("Proizved ciph: ")
-        proizvedOut n
-        Console.WriteLine("Min ciph: ")
-        minOut n
-        0
-        *)
