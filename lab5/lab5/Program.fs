0b9ce930b33ccf9b96b9fb0e32c847072d35c567
Дмитрий Александрович Худокормов <s0163526@edu.kubsu.ru>
2022-04-03 09:06:49 +0300

    task12

--- lab5/lab5/Program.fs
+++ lab5/lab5/Program.fs
@@ -16,3 +16,24 @@ let main argv =
     printfn "%s" (prog11 s)
     0 
 *)
+
+(* 12.
+[<EntryPoint>]
+let main argv =
+    printfn "Какой язык нраица?"
+    let Ans ans = 
+        match ans with 
+        |"F#"|"Prolog"->"Подлиза :), но я тебя уважаю"
+        |"Python"|"R"->"Я с тобой не разговариваю."
+        |_->"Ааа... Понятно!"
+
+    //carring
+
+    //Console.ReadLine()|> Ans |>printfn "%s" 
+
+    //superposition
+    let kostbILFunc arg = (if arg=4 then "F#" else "R")
+    let funcForSuper = kostbILFunc>>Ans>>printfn "%s"
+    funcForSuper 4
+    0
+    *)
